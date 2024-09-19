3c7e694d31ae563dcb284a04a808ba732ed37f46
Nibras S <160467523+Nibras-S@users.noreply.github.com>
2024-09-19 20:17:42 +0530

    first commit

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import car1 from '../assets/car1.jpg'
+import car2 from '../assets/car2.jpg'
+import car3 from '../assets/car3.jpg'
 
 const Carousel = () => {
   const settings = {
@@ -20,7 +23,7 @@ const Carousel = () => {
       <div className="container mx-auto w-full">
         <Slider {...settings}>
           <div className="relative">
-            <img src="/images/car1.jpg" alt="Car 1" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
+            <img src={car1} alt="Car 1" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
               <h2 className="text-2xl md:text-4xl font-bold mb-4">Customize, Upgrade, and Replace</h2>
               <a href="/contact" className="bg-yellow-500 text-black py-2 px-4 rounded-lg hover:bg-yellow-600">Contact Us</a>
@@ -28,7 +31,7 @@ const Carousel = () => {
           </div>
 
           <div className="relative">
-            <img src="/images/car2.jpg" alt="Car 2" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
+            <img src={car2} alt="Car 2" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
               <h2 className="text-2xl md:text-4xl font-bold mb-4">Customize, Upgrade, and Replace</h2>
               <a href="/contact" className="bg-yellow-500 text-black py-2 px-4 rounded-lg hover:bg-yellow-600">Contact Us</a>
@@ -36,7 +39,7 @@ const Carousel = () => {
           </div>
 
           <div className="relative">
-            <img src="/images/car3.jpg" alt="Car 3" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
+            <img src={car3} alt="Car 3" className="w-full h-64 md:h-96 lg:h-[32rem] object-cover" />
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-50">
               <h2 className="text-2xl md:text-4xl font-bold mb-4">Customize, Upgrade, and Replace</h2>
               <a href="/contact" className="bg-yellow-500 text-black py-2 px-4 rounded-lg hover:bg-yellow-600">Contact Us</a>
