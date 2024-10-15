818bda4fbb57c06b9fcbeca613700ff49c85372d
Nibras S <160467523+Nibras-S@users.noreply.github.com>
2024-10-15 21:01:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Return from './Pages/Return';
+import Faqs from './Pages/Faqs'
 import RouteTop from './components/RouteTop';
 import { Route, Routes } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/aboutus' element={<About />} />
         <Route path='/' element={<Home />} />
         <Route path='/returns' element={<Return />} />
+        <Route path='/faqs' element={<Faqs />} />
       </Routes>
     </div>
   );
